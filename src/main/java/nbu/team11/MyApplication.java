2534f8d1bed4861e8950b78433d07a4db184805f
Ivanka Todorova <iv@itodorova.dev>
2024-11-20 14:07:43 +0200

    chore: initial setup (spring boot, thymeleaf with gradle) #3

---
+++ src/main/java/nbu/team11/MyApplication.java
@@ -0,0 +1,38 @@
+package nbu.team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// uncomment if you want to return a string/json
+//import org.springframework.web.bind.annotation.RestController;
+
+
+// uncomment if you want to return a string/json
+//@RestController
+@Controller
+@SpringBootApplication
+public class MyApplication {
+
+// uncomment if you want to return a string/json
+
+//    @RequestMapping("/")
+//    String home() {
+//        return "Hello World!";
+//    }
+
+    @GetMapping("/hello")
+    String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
