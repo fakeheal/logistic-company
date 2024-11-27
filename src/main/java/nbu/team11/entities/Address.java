25b1107ce0c95547fb968935a04acd1b1af9e5be
EliTsekova <onoka@abv.bg>
2024-11-27 14:43:28 +0200

    Add new classes AddressType, Shipment, Shipment_Status, Status and modified class Address

--- src/main/java/nbu/team11/entities/Address.java
+++ src/main/java/nbu/team11/entities/Address.java
@@ -1,10 +1,9 @@
 package nbu.team11.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import nbu.team11.entities.enums.AddressType;
+
 
 @Entity
 @Data
@@ -12,4 +11,17 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "CityId", nullable = false)
+    private City city;
+
+    @Column(name = "Street", nullable = false)
+    private String street;
+
+    @Column(name = "PostalCode", nullable = false)
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "AddressType", nullable = false)
+    private AddressType addressType;
 }
---
+++ src/main/java/nbu/team11/entities/Shipment_Status.java
@@ -0,0 +1,27 @@
+package nbu.team11.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import nbu.team11.entities.enums.Status;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Shipment_Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "ShipmentId", nullable = false)
+    private Shipment shipment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Status", nullable = false)
+    private Status status;
+
+    @Column(name = "CreatedOn", nullable = false)
+    private LocalDateTime createdOn;
+}
