4c1b6a75789f7bf4223698b220aa1185ff368c26
EliTsekova <onoka@abv.bg>
2024-11-27 14:43:28 +0200

    Add new classes AddressType, Shipment, Shipment_Status, Status and modified class Address

---
+++ src/main/java/nbu/team11/entities/Shipment.java
@@ -0,0 +1,50 @@
+package nbu.team11.entities;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+import lombok.Data;
+import nbu.team11.entities.Address;
+import nbu.team11.entities.Office;
+import nbu.team11.entities.Employee;
+import nbu.team11.entities.City;
+import nbu.team11.entities.Client;
+
+@Entity
+@Data
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "EmployeeId", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "RecipientId", nullable = false)
+    private Client recipient;
+
+    @ManyToOne
+    @JoinColumn(name = "RecipientAddress", nullable = false)
+    private Address recipientAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "SenderId", nullable = false)
+    private Client sender;
+
+    @ManyToOne
+    @JoinColumn(name = "SenderAddress", nullable = false)
+    private Address senderAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "OfficeId", nullable = false)
+    private Office office;
+
+    @Column(name = "Weight", nullable = false)
+    private double weight;
+
+    @Column(name = "Price", nullable = false)
+    private BigDecimal price;
+}
