88f6c68b37ec7634d5ac90cb7eee2687d2820cb1
Ivanka Todorova <iv@itodorova.dev>
2024-12-04 10:20:46 +0200

    chore: restructure template hierarchy

--- src/main/java/nbu/team11/DemoPages.java
+++ src/main/java/nbu/team11/DemoPages.java
@@ -15,42 +15,42 @@ public class DemoPages {
     @GetMapping("/demo")
     public String demo(Model model) {
         model.addAttribute("title", "Demo Page");
-        model.addAttribute("content", "demo/index");
+        model.addAttribute("content", "demo/pages/index");
         return withAppLayout(model);
     }
 
     @GetMapping("/demo/login")
     public String login(Model model) {
         model.addAttribute("title", "Login Page");
-        model.addAttribute("content", "demo/login");
+        model.addAttribute("content", "demo/pages/guest/login");
         return withAppLayout(model);
     }
 
     @GetMapping("/demo/register")
     public String register(Model model) {
         model.addAttribute("title", "Register Page");
-        model.addAttribute("content", "demo/register");
+        model.addAttribute("content", "demo/pages/guest/register");
         return withAppLayout(model);
     }
 
     @GetMapping("/demo/forgotten-password")
     public String forgottenPassword(Model model) {
         model.addAttribute("title", "Forgotten password");
-        model.addAttribute("content", "demo/forgotten-password");
+        model.addAttribute("content", "demo/pages/guest/forgotten-password");
         return withAppLayout(model);
     }
 
     @GetMapping("/demo/reset-password")
     public String resetPassword(Model model) {
         model.addAttribute("title", "Reset password");
-        model.addAttribute("content", "demo/reset-password");
+        model.addAttribute("content", "demo/pages/guest/reset-password");
         return withAppLayout(model);
     }
 
     @GetMapping("/demo/track")
     public String track(Model model) {
         model.addAttribute("title", "Track package");
-        model.addAttribute("content", "demo/track");
+        model.addAttribute("content", "demo/pages/track");
         return withAppLayout(model);
     }
 
