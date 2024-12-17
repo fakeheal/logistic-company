7991b66bec5cc2fb730eab066a6d077b87ee6a2a
yoanna-gicheva <yoanna.gicheva@mail.com>
2024-12-17 12:19:03 -0300

    refactor(CustomUserDetailsService): change loadUserByUsername implementation to work with email

--- src/main/java/nbu/team11/services/CustomUserDetailsService.java
+++ src/main/java/nbu/team11/services/CustomUserDetailsService.java
@@ -20,8 +20,12 @@ public class CustomUserDetailsService  implements UserDetailsService{
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
 
         //TODO: Check how password is handled
         return new CustomUserDetails(
