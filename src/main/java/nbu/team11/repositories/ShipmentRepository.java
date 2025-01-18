bbd41974035a09b067a1e98af4211737e5653638
EliTsekova <onoka@abv.bg>
2025-01-18 11:39:02 +0200

    Modified repositories for Shipment, City and Country

--- src/main/java/nbu/team11/repositories/ShipmentRepository.java
+++ src/main/java/nbu/team11/repositories/ShipmentRepository.java
@@ -1,7 +1,17 @@
 package nbu.team11.repositories;
 
 import nbu.team11.entities.Shipment;
+import nbu.team11.entities.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
+    List<Shipment> findAllByOrderByCreatedOnDesc();
+    List<Shipment> findAllByEmployeeId(Integer employeeId);
+    @Query("SELECT s FROM Shipment s WHERE s.id NOT IN (SELECT ss.shipment.id FROM ShipmentStatus ss WHERE ss.status = :deliveredStatus)")
+    List<Shipment> findUndeliveredShipments(@Param("deliveredStatus") Status deliveredStatus);
+
 }
